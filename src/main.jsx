337259d5c6fb480f0daa38a545e6d83af38c33d2
sowmik-sec <sowmik.sec@gmail.com>
2024-07-01 21:18:00 +0600

    set basic routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Donation from "./components/Donation/Donation";
+import Statistics from "./components/Statistics/Statistics";
+import Root from "./components/Root/Root";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/donation",
+        element: <Donation />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
