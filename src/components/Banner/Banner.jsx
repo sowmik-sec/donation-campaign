8486c72e8bc7a895354168f141b13ccf180c0a1a
sowmik-sec <sowmik.sec@gmail.com>
2024-07-02 12:17:35 +0600

    remove unnecessary width

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ function Banner() {
     <div
       className={`bg-[url('https://www.habitatswmt.org/wp-content/uploads/2021/04/Donate.jpg')] bg-no-repeat bg-cover w-full h-[600px]`}
     >
-      <div className="text-center relative top-56 bg-white h-4/6 opacity-95 space-y-7 w-[99%]">
+      <div className="text-center relative top-56 bg-white h-4/6 opacity-95 space-y-7 ">
         <h2 className="text-5xl text-black ">
           I Grow By Helping People In Need
         </h2>
