67704b0ce3d87f463f3096f15844342cf4501ebf
sowmik-sec <sowmik.sec@gmail.com>
2024-07-02 21:11:35 +0600

    click to show details

--- src/components/ShowDonations/ShowDonations.jsx
+++ src/components/ShowDonations/ShowDonations.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import DonationCard from "../DonationCard/DonationCard";
+import { Link } from "react-router-dom";
 
 function ShowDonations() {
   const [donations, setDonations] = useState([]);
@@ -11,7 +12,14 @@ function ShowDonations() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       {donations.map((donation) => (
-        <DonationCard key={donation.id} donation={donation} />
+        <Link
+          key={donation.id}
+          to={`/donation-details/${donation.id}`}
+          state={{ donation }}
+          className="cursor-pointer"
+        >
+          <DonationCard donation={donation} />
+        </Link>
       ))}
     </div>
   );
