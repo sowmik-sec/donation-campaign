759585ab844e1c119376944eab01358e7b16f7a4
sowmik-sec <sowmik.sec@gmail.com>
2024-07-02 22:25:02 +0600

    show donations

---
+++ src/components/Donation/Donation.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { getLocalStorageData } from "../../utilities/localStorage";
+import DonateCard from "../DonateCard/DonateCard";
+
+function Donation() {
+  const donations = getLocalStorageData();
+  const [showItems, setShowItems] = useState(4);
+  const handleShowAll = () => {
+    setShowItems(donations.length);
+  };
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
+        {donations.slice(0, showItems).map((donation) => (
+          <DonateCard key={donation.id} donation={donation} />
+        ))}
+      </div>
+      <div
+        className={`text-center mt-10 ${showItems > 4 ? "hidden" : "block"}`}
+      >
+        <button
+          onClick={handleShowAll}
+          className="text-white bg-green-400 px-4 py-2 rounded-md"
+        >
+          See All
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Donation;
